b7cf8a6004808b6b2e5ed52ee240012f9f7e6ae4
Edmundas Danėnas <edmundas.danenas@ktu.edu>
2022-12-18 17:21:57 +0200

    edit

--- client/pages/admin-matches/edit.vue
+++ client/pages/admin-matches/edit.vue
@@ -12,38 +12,22 @@
               <div class="card-body">
                 <label for="name">Match name</label>
                 <input type="text" name="name" placeholder="" class="form-control mb-2">
-                <p><input type="checkbox" id="checkbox" v-model="autoCoff" />
-                  <label for="checkbox"> Automatic odds calculation</label>
-                </p>
-                <div class="row">
-                  <div class="col-sm">
-                    <button @click='isShowingOutcome = !isShowingOutcome' class="btn btn-primary btn-block mt-3"
-                      name="createMatch">Add match outcome</button>
-                  </div>
-                </div>
-                <div id="hide" v-show='isShowingOutcome' class="col-sm">
+                <label style="margin-top:1rem" for="outcomeselection">Select match outcome</label>
+                <select v-on:change="onChangeSite($event)" class="form-select form-select-sm" aria-label=".form-select-sm example">
+                  <option value="null" selected></option>
+                  <option value="1">One</option>
+                  <option value="2">Two</option>
+                  <option value="3">Three</option>
+                </select>
+                <div style="margin-top:1rem;" id="hide" v-if='isShowingOutcome' class="col-sm">
                   <div class="row">
                     <div class="col-sm">
-                      <label for="name">Outcome name</label>
-                      <input type="text" name="name" placeholder="" class="form-control mb-2">
                       <label for="name">Odds team A</label>
                       <input type="number" name="name" placeholder="" class="form-control mb-2">
                       <label for="name">Odds team B</label>
                       <input type="number" name="name" placeholder="" class="form-control mb-2">
-                      <button @click='isShowingOutcome = !isShowingOutcome' class="btn btn-primary btn-block mt-3"
-                        name="createMatch">Create match outcome</button>
                     </div>
                   </div>
-                </div>
-                <label style="margin-top:1rem" for="outcomeselection">Match outcomes</label>
-                <div class="overflow-auto" style="width:20rem;height:10rem">
-                  <ul class="list-group">
-                    <li class="list-group-item">Outcome1</li>
-                    <li class="list-group-item">Outcome2</li>
-                    <li class="list-group-item">Outcome3</li>
-                    <li class="list-group-item">Outcome4</li>
-                    <li class="list-group-item">Outcome5</li>
-                  </ul>
                 </div>
                 <button class="btn btn-success btn-block mt-3" name="createMatch">Save match</button>
               </div>
@@ -62,10 +46,18 @@ const router = useRouter()
 const route = useRoute()
 console.log(router.resolve(route))
 //
-const autoCoff = ref<boolean>(false)
-const isShowingOutcome = ref<boolean>(false)
+let isShowingOutcome = ref<boolean>(false);
 
-
+function onChangeSite(e){
+  if(e.target.value != "null")
+  {
+     isShowingOutcome.value = true;
+  }
+  else
+  {
+    isShowingOutcome.value = false;
+  }
+}
 </script>
 
 
