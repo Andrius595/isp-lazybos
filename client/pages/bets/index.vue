20795d288dd40ee2019803a87c1ea94ba798f022
Andrius <and.cereska@gmail.com>
2022-12-19 00:36:10 +0200

    Users bets table

--- client/pages/bets/index.vue
+++ client/pages/bets/index.vue
@@ -1,18 +1,50 @@
 <template>
   <div>
     <AuthenticatedLayout>
-      <h1>Statymų sąrašas</h1>
+      <h1>Bets list</h1>
+
+      <table class="table table-striped">
+        <thead>
+        <tr>
+          <th scope="col">Sum</th>
+          <th scope="col">Date</th>
+          <th scope="col">Coefficient</th>
+          <th scope="col">Bet status</th>
+          <th scope="col">Selection winner</th>
+        </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(bet, index) in bets" :key="index">
+            <td>{{ bet.stake }} Eur</td>
+            <td>{{ formatDate(bet.timestamp) }}</td>
+            <td>{{ bet.odds }}</td>
+            <td>{{ bet.status }}</td>
+            <td>{{ bet.selection.winner }}</td>
+          </tr>
+        </tbody>
+      </table>
     </AuthenticatedLayout>
   </div>
 </template>
 
 <script setup lang="ts">
 import AuthenticatedLayout from "~/layouts/AuthenticatedLayout.vue";
+import formatDate from "~/utils/formatDate";
 
+
+const bets = ref([])
 await fetchBets()
 
 async function fetchBets() {
-  await $fetch('/api/events/user-bets', { method: 'POST' })
+  const response = await $fetch('/api/events/user-bets', { method: 'POST' })
+
+  if (!response.status) {
+    // TODO show error
+
+    return
+  }
+
+  bets.value = response.data
 }
 </script>
 <style scoped lang="scss">
