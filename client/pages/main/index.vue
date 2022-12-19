a7e1d2554dacaf06b0ef7f2a72ff391083304ef4
Andrius <and.cereska@gmail.com>
2022-12-19 14:17:47 +0200

    Main page update.

--- client/pages/main/index.vue
+++ client/pages/main/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <AuthenticatedLayout>
-      <h1>Pagrindinis langas</h1>
+      <h1 v-if="user">Hello, {{user.first_name}}</h1>
     </AuthenticatedLayout>
   </div>
 </template>
@@ -13,6 +13,8 @@ import {definePageMeta} from "#imports";
 definePageMeta({
   middleware: 'logged-in',
 })
+
+const user = useCookie('user')
 </script>
 
 <style scoped lang="scss">
